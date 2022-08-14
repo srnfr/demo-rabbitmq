ba7739ec5c03fb59ac413c0797766d4175189125
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-14 17:29:34 +0200

    Update rpc_server.php

--- php/rpc_server.php
+++ php/rpc_server.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+$ini = parse_ini_file('config.ini');
 $connection = new AMQPStreamConnection($ini['server'], $ini['port'], $ini['username'], $ini['password'], $ini['vhost']);
 $channel = $connection->channel();
 
