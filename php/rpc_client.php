332eb256ff61995f81a10d5a3d0e6bd5729d374c
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-14 17:29:59 +0200

    Update rpc_client.php

--- php/rpc_client.php
+++ php/rpc_client.php
@@ -4,6 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+
+$ini = parse_ini_file('config.ini');
+
 class FibonacciRpcClient
 {
     private $connection;
