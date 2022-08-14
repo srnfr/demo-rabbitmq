dcac95ed4ba8c36b39c342e3191a1a9d174ff7a2
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-14 17:24:56 +0200

    Update receive.php

--- php/receive.php
+++ php/receive.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection($ini['server'], $ini['port'], $ini['username'], $ini['password'], $ini['vhost']);
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
